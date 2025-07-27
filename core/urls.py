264789609a3dac4365233782f7277594df55de9a
obonyodorice <obonyodorice3@gmail.com>
2025-07-26 21:50:59 -0700

    cm

--- core/urls.py
+++ core/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import index
+from .views import index, about_view, contact_view
 
 urlpatterns = [
-    path('', index, name='index'),  # Home page
+    path('', index, name='index'), 
+    path('about/', about_view, name='about'),
+    path('contact/',contact_view, name='contact'), 
     path('admin/', admin.site.urls),
     path('venue/',include('venue.urls')), 
     path('bookings/', include('bookings.urls')),  # Include URLs from the venue app
